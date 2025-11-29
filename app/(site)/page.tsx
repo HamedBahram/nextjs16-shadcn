389ed434ae1f1d490a18277453fac04abfc3e54b
Hamed Bahram <hamed.bahram@gmail.com>
2025-11-29 08:20:19 -0500

    Update packages

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className='py-32'>
       <div className='container'>
-        <h1 className='text-3xl font-semibold'>NextJs 16 + Shadcn</h1>
+        <h1 className='text-3xl font-semibold'>NextJs 16 + shadcn</h1>
       </div>
     </section>
   )
