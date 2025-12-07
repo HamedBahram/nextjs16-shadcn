bc0aa9a548ae3ee3ea3a666bdbe064640c85101f
Hamed Bahram <hamed.bahram@gmail.com>
2025-12-07 02:43:25 -0500

    enable cache components

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,20 @@
 import Link from 'next/link'
-import { Button } from './ui/button'
+import { Suspense } from 'react'
+
+import { Button } from '@/components/ui/button'
+import { CopyrightYear } from '@/components/copyright-year'
 
 export default function Footer() {
   return (
     <footer className='py-4'>
       <div className='text-muted-foreground container flex max-w-7xl flex-col items-center justify-between gap-x-3 gap-y-1 text-center text-sm sm:flex-row'>
-        <p>&copy;{new Date().getFullYear()} NextJs 16</p>
+        <p>
+          &copy;
+          <Suspense fallback={<span>----</span>}>
+            <CopyrightYear />
+          </Suspense>{' '}
+          NextJs 16
+        </p>
         <p>
           Template by{' '}
           <Button variant='link' className='p-0' asChild>
